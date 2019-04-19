94d1b3487b5023d60a80b9073388f4ddee579314
Vadim Klimenko <tehnomall.by@gmail.com>
2019-04-19 11:09:47 +0300

    Practicing with generics

---
+++ Practice/Task 1/Generics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 10;
+            int b1 = 20;
+
+            string a2 = "Hello ";
+            string b2 = "World!";
+
+
+            Console.WriteLine("before swap: {0}, {1}", a1,b1);
+            Console.WriteLine("before swap: {0}, {1}", a2, b2);
+
+            Swap<int>(ref a1, ref b1);
+            Swap<string>(ref a2, ref b2);
+
+
+            Console.WriteLine("after swap: {0}, {1}", a1, b1);
+            Console.WriteLine("after swap: {0}, {1}", a2, b2);
+
+
+        }
+
+        static void Swap <T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
